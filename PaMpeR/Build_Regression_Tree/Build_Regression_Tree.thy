9a451110a2996f29a471c31795bb03f8e5482214
Yutaka Nagashima <yutacang@googlemail.com>
2018-01-25 18:43:03 +0100

    fix: add a missing theory to Build_Regression_Tree.
    This should have been done in the last commit.

---
+++ PaMpeR/Build_Regression_Tree/Build_Regression_Tree.thy
@@ -0,0 +1,15 @@
+theory Build_Regression_Tree
+  imports "../PaMpeR"
+  keywords "build_regression_trees" :: thy_decl
+  and      "print_out_regression_trees" :: thy_decl
+begin
+
+ML_file "../Assertions.ML"
+
+reset_regression_tree_table
+
+build_regression_trees
+
+print_out_regression_trees
+
+end
