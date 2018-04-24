4e5ddb4255810909558d4c35ca63459af8512370
Yutaka Nagashima <yutacang@googlemail.com>
2018-04-24 17:27:54 +0200

    improving postprocessing.

--- PaMpeR/Postprocess/Postprocess.thy
+++ PaMpeR/Postprocess/Postprocess.thy
@@ -19,6 +19,7 @@ val proc             = Bash.process;
 val path             = Resources.master_directory @{theory} |> File.platform_path : string;
 val path_to_PaMpeR   = unsuffix "/Postprocess" path;
 val path_to_all_meth_names = path_to_PaMpeR ^ "/Preprocess/method_names": string;
+val path_to_all_meth_namees_standard = path_to_PaMpeR ^ "/Preprocess/method_names_in_standard_library": string;
 val path_to_Database = path_to_PaMpeR ^ "/Build_Database/Database":string;
 val eval_file        = path_to_PaMpeR ^ "/Evaluate_ASE/evaluation_success.txt";
 fun thr_dig (r:real) = ((r * 1000.0) |> Real.round |> Real.fromInt |> (fn x => x / 10.0)):real;
@@ -37,6 +38,17 @@ val all_method_names =
   in
     dist_meth_names : string list
   end;
+
+val all_method_names_standard =
+  let
+    val bash_script = "while read line \n do echo $line | awk '{print $1;}' \n done < '" ^ path_to_all_meth_namees_standard ^ "'" : string;
+    val bash_input  = Bash.process bash_script |> #out : string;
+    val dist_meth_names = bash_input |> String.tokens (fn c => c = #"\n") |> distinct  (op =);
+  in
+    dist_meth_names : string list
+  end;
+
+val all_method_names_user_defined = subtract (op =) all_method_names_standard all_method_names;
 
 fun postprocess_one_meth (_    :string) (0:int) (_:int) (_              )  (numbs:real option list) = numbs
  |  postprocess_one_meth (mname:string) (n:int) (t:int) (accm:real option) (numbs:real option list) =
@@ -66,33 +78,55 @@ fun postprocess_one_meth' (n:int) (mname:string) =
       then map the maybe_numbs
       else (tracing ("postprocess_all_meths partially failed. Some Nones were detected for " ^ mname);[]):int list;
     val numbs_strs = map Int.toString numbs: string list;
-    val total_occr = proc ("grep '\\b" ^ mname ^ "\\b' " ^ path_to_Database ^ " | wc | awk '{print $1;}'") |> #out |> Int.fromString |> the |> Int.toString;
-    val eval_occr  = proc ("grep '^" ^ mname ^ "\\s' " ^ eval_file ^ " | wc | awk '{print $1;}'") |> #out |> Int.fromString |> the |> Int.toString;
-    val total_real_train = proc ("grep '\\b" ^ mname ^ "\\b' " ^ path_to_Database ^ " | wc | awk '{print $1;}'") |> #out |> Int.fromString |> the |> Real.fromInt;
+    val occur_in_train = if mname = "-"
+      then proc ("grep ' - ' " ^ path_to_Database ^ " | wc | awk '{print $1;}'") |> #out |> Int.fromString |> the
+      else proc ("grep '\\b" ^ mname ^ "\\b' " ^ path_to_Database ^ " | wc | awk '{print $1;}'") |> #out |> Int.fromString |> the;
+    val occur_in_train_str = Int.toString occur_in_train;
+    val occur_in_eval  = proc ("grep '^" ^ mname ^ "\\s' " ^ eval_file ^ " | wc | awk '{print $1;}'") |> #out |> Int.fromString |> the |> Int.toString;
+    val occur_in_train_real = Real.fromInt occur_in_train;
     val total_real_test = proc ("grep '^" ^ mname ^ "\\s' " ^ eval_file ^ " | wc | awk '{print $1;}'") |> #out |> Int.fromString |> the |> Real.fromInt;
-    val freqency_train = thr_dig (total_real_train / datasize_real) |> Real.toString: string;
+    val freqency_train = thr_dig (occur_in_train_real / datasize_real) |> Real.toString: string;
     val frequency_test = thr_dig (total_real_test /  testsize_real) |> Real.toString: string;
-    val line = "'\\verb|" ^ mname ^ "| & " ^ total_occr ^ " & " ^ freqency_train ^ " & " ^ eval_occr ^ " & " ^ frequency_test ^ " & " ^
+    val line_latex = "'\\verb|" ^ mname ^ "| & " ^ occur_in_train_str ^ " & " ^ freqency_train ^ " & " ^ occur_in_eval ^ " & " ^ frequency_test ^ " & " ^
             (space_implode " & " numbs_strs) ^ "\\" ^ "\\" ^ "'";
+    val line_raw = "'[(*" ^ mname ^ "*) " ^ (space_implode "," numbs_strs) ^ "]'";
   in
-    (if null numbs then "" else line)
+    (if null numbs then ("", "") else (line_latex, line_raw))
   end;
 
-fun postprocess_all_meth n mnames = Par_List.map (postprocess_one_meth' n) mnames: string list;
+fun postprocess_all_meth n mnames = Par_List.map (postprocess_one_meth' n) mnames: (string * string) list;
 
-fun print_one_result (line:string) = proc ("echo " ^ line ^ " >> " ^ path ^ "/eval_result.txt");
+fun print_one_result (dest:string) (line:string) = proc ("echo " ^ line ^ " >> " ^ path ^ dest);
+
+val among_top_nth = 169:int;
 
 in
 
- postprocess_all_meth 15 all_method_names |> map print_one_result
+ postprocess_all_meth among_top_nth all_method_names_standard |> map (fn (latex, raw) => (
+(*
+   print_one_result "/eval_result_in_standard_latex.txt" latex;
+*)
+   print_one_result "/eval_result_in_standard_raw.txt" raw));
+
+ postprocess_all_meth among_top_nth all_method_names_user_defined |> map (fn (latex, raw) => (
+(*
+   print_one_result "/eval_result_user_defined_latex.txt" latex;
+*)
+   print_one_result "/eval_result_user_defined_raw.txt" raw));
+
+ postprocess_all_meth among_top_nth all_method_names |> map (fn (latex, raw) => (
+(*
+   print_one_result "/eval_result_all_methods_latex.txt" latex;
+*)
+   print_one_result "/eval_result_all_methods_raw.txt" raw))
 
 end;
 *}
-
+(*
 ML{*
 postprocess ();
 *}
-
+*)
 (* Consider using sort -k3q -nr eval_result.txt *)
 
 end
