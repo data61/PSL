1c87cdc022c19d00a8c4eda8fb35b70e91ac5138
Yutaka Nagashima <yutacang@googlemail.com>
2018-09-02 19:09:16 +0200

    rewrite Kei's proof in an automation friendly manner.

--- MiLkMaId/TIP_with_Proof/Prod/Prod/TIP_prop_13.thy
+++ MiLkMaId/TIP_with_Proof/Prod/Prod/TIP_prop_13.thy
@@ -6,7 +6,7 @@
      https://github.com/tip-org/benchmarks 
    Yutaka Nagashima at CIIRC, CTU changed the TIP output theory file slightly 
    to make it compatible with Isabelle2017.
-   Some proofs were added by Yutaka Nagashima.*)
+   Some proofs were added by Yutaka Nagashima and Kei Shirakizawa.*)
   theory TIP_prop_13
   imports "../../Test_Base"
 begin
@@ -22,13 +22,78 @@ fun t2 :: "Nat => Nat => Nat" where
 "t2 (Z) y = y"
 | "t2 (S z) y = S (t2 z y)"
 
-lemma t2_succ: "t2 n (S m) = t2 (S n) m"
-  by(induction n, auto)
-
 theorem property0 :
   "((half (t2 x x)) = x)"
-  apply(induct x rule: half.induct, auto)
-  apply(simp add:t2_succ)
+  apply(induct x rule: half.induct)
+    apply fastforce+
+  (*Kei conjectured "\<And>n m. t2 n (S m) = t2 (S n) m"*)
+  apply(subgoal_tac "\<And>n m. t2 n (S m) = t2 (S n) m")
+   apply fastforce
+  apply(thin_tac "half (t2 z z) = z")
+  apply(induct_tac n)
+   apply auto
   done
+
+(*Conjecturing "\<And>n m. t2 n (S m) = t2 (S n) m" from
+ *"\<And>z. half (t2 z z) = z \<Longrightarrow> half (t2 (S (S z)) (S (S z))) = S (S z)" is a hard task for machines.
+ *In the following, we take a step-by-step approach.*)
+theorem property04 :
+  "((half (t2 x x)) = x)"
+  apply(induct x rule: half.induct)
+    apply fastforce+
+  apply clarsimp
+  (*Since "z" and "(S (S z))" in "t2 z (S (S z))" are of the same type and
+   *"t2"'s pattern-matching is complete on the first argument...*)
+  apply(subgoal_tac "(t2 z (S (S z))) = (t2 (S (S z)) z)")
+   apply fastforce
+  (*Since "S z" appears twice in "t2 z (S (S z)) = t2 (S (S z)) z", we generalize it.*)
+  apply(subgoal_tac "\<And>z Sz. t2 z (S Sz) = t2 (S Sz) z")
+   apply fastforce
+  apply(thin_tac "half (t2 z z) = z")
+  apply(induct_tac za)
+   apply clarsimp
+   apply(induct_tac Sz)
+    apply fastforce+
+  apply clarsimp
+  apply(induct_tac Sz)
+   apply fastforce+
+  done
+
+theorem property01 :
+  "((half (t2 x x)) = x)"
+  apply(induct x rule: half.induct)
+    apply fastforce+
+    (* "(t2 (S (S z)) (S (S z)))" in "half (t2 (S (S z)) (S (S z))) = S (S z)" is problematic:
+   * t2's pattern matching is complete only on the first argument.
+   * So, we want to rewrite "(t2 (S (S z)) (S (S z)))" to "t2 (S (S (S (S z))))".
+   * Furthermore, "half"'s last clause can simplify "t2 (S (S (S (S z))))" very well. *)
+  apply(subgoal_tac "(t2 (S (S z)) (S (S z))) = t2 (S (S (S (S z)))) z")
+   apply fastforce
+  apply clarsimp
+  apply(thin_tac "half (t2 z z) = z")
+  apply(subgoal_tac "t2 z (S (S z)) = t2 (S (S z)) z")
+   apply fastforce
+  apply(subgoal_tac "\<And>z Sz. t2 z (S Sz) = t2 (S Sz) z")
+   apply fastforce
+  apply(induct_tac za)
+   apply clarsimp
+   apply(induct_tac Sz)
+    apply fastforce+
+  apply clarsimp
+  apply(induct_tac Sz)
+   apply auto
+  done
+
+theorem property02 :
+  "((half (t2 x x)) = x)"
+  apply(induct x x rule:t2.induct)thm t2.induct
+  quickcheck
+  oops (*The induction step "\<And>z y. half (t2 y y) = y \<Longrightarrow> half (t2 y y) = y" is not useful at all.*)
+
+theorem property03 :
+  "((half (t2 x x)) = x)"
+  apply(induct rule:t2.induct)thm t2.induct
+  quickcheck
+  oops
 
 end
