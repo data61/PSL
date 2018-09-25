0165d1763a6a03343a8778931aa9f3054c54e387
Yutaka Nagashima <yutacang@googlemail.com>
2018-09-25 16:04:00 +0200

    rename a MiLkMaId_Assertion.ML to MiLkMaId_Util.ML.
    Assertions are now in Smart_Induct.ML

--- MiLkMaId/src/MiLkMaId.thy
+++ MiLkMaId/src/MiLkMaId.thy
@@ -1,11 +1,7 @@
 theory MiLkMaId
-  imports MiLkMaId_Util MiLkMaId_Example
+  imports MiLkMaId_Example
 begin
 
-ML_file "MiLkMaId_Assertion.ML"
-
-ML{* (* TODO: to differentiate "inductive" and "inductive_set", we should check the types.  *)
-dest_Const @{term "even_set"} |> snd |> dest_Type |> fst;
-*}
+ML_file "MiLkMaId_Util.ML"
 
 end
