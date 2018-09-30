3862a89297856e25f829643708d0baadd4288697
Yutaka Nagashima <yutacang@googlemail.com>
2018-10-01 00:03:10 +0200

    fix

--- MiLkMaId/TIP_with_Proof/Prod/Prod/TIP_prop_04.thy
+++ MiLkMaId/TIP_with_Proof/Prod/Prod/TIP_prop_04.thy
@@ -30,7 +30,6 @@ fun double :: "Nat => Nat" where
 theorem property0 :
   "((length (x y y)) = (double (length y)))"
   (*why not "induct y"?*)
-  apply(induct rule:x.induct)
   apply(induct rule:length.induct)
    apply auto[1]
   apply clarsimp
