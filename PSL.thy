79cb7294cf46ec88e8900168be0cf1c2d8a34874
Yutaka Nagashima <yutacang@googlemail.com>
2018-11-07 16:03:01 +0100

    add exist_n and exist_mult, which are necessary to develop ass_021 and ass_022.

--- PSL.thy
+++ PSL.thy
@@ -14,6 +14,4 @@ text{* Uncomment the following to unleash the power parallelism. *}
 ML{* Multithreading.max_threads_update 28 *}
 ML{* Multithreading.parallel_proofs := 0; *}
 
-ML{* @{term "x::'a list"} |> Term.dest_Free |> snd |> Term.dest_Type *}
-ML{* @{term "x::'a set"} |> Term.dest_Free |> snd |> Term.dest_Type *}
 end
