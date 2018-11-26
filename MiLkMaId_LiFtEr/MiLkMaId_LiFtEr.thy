3ffaf47d798ecc6c652c42d8b998bd253bde8e69
Yutaka Nagashima <yutacang@googlemail.com>
2018-11-26 22:13:51 +0100

    add signatures for MiLkMaId and LiFtEr.

--- MiLkMaId_LiFtEr/MiLkMaId_LiFtEr.thy
+++ MiLkMaId_LiFtEr/MiLkMaId_LiFtEr.thy
@@ -1,5 +1,23 @@
+(*  Title:      PSL/MiLkMaId_LiFtEr.thy
+    Author:     Yutaka Nagashima, Czech Technical University in Prague, the University of Innsbruck
+
+MiLkMaId: Machine Learning Mathematical Induction for Isabelle/HOL, and
+LiFtEr:   Logical Feature Extractor.
+*)
 theory MiLkMaId_LiFtEr
   imports Main
 begin
+
+ML{* (*type synonyms*)
+type strings = string list;
+type ints    = int    list;
+*}
+
+ML_file "Transform_Sig.ML"
+ML_file "Transform_Struct.ML"
+ML_file "LiFtEr_Sig.ML"
+ML_file "LiFtEr_Struct.ML"
+ML_file "DInduct_Sig.ML"
+ML_file "DInduct_Struct.ML"
 
 end
