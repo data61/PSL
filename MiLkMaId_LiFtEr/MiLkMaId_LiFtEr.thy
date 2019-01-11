62ad27f43e35ef741bae7b25a41cb6c6ff61abbe
Yutaka Nagashima <yutacang@googlemail.com>
2019-01-11 01:34:50 +0100

    add utility functions for matrix computation.

--- MiLkMaId_LiFtEr/MiLkMaId_LiFtEr.thy
+++ MiLkMaId_LiFtEr/MiLkMaId_LiFtEr.thy
@@ -22,6 +22,11 @@ val should_be_question_Q = @{thm conjI} |> Thm.concl_of |> Term.dest_comb |> snd
 @{assert} ("?Q" = should_be_question_Q);
 *}
 
+ML_file "../MiLkMaId/src/MiLkMaId_Util.ML"
+ML_file "Matrix_Sig.ML"
+ML_file "Matrix_Struct.ML"
+ML_file "LiFtEr_Util_Sig.ML"
+ML_file "LiFtEr_Util_Struct.ML"
 ML_file "Unique_Node_Sig.ML"
 ML_file "Unique_Node_Struct.ML"
 ML_file "Unique_Node_Test.ML"
@@ -30,8 +35,6 @@ ML_file "Term_Table_Struct.ML"
 ML_file "Term_Table_Test.ML"
 ML_file "DInduct_Sig.ML"
 ML_file "DInduct_Struct.ML"
-ML_file "LiFtEr_Util_Sig.ML"
-ML_file "LiFtEr_Util_Struct.ML"
 ML_file "LiFtEr_Sig.ML"
 ML_file "LiFtEr_Struct.ML"
 
