b63233e800f8d410dbf713eb6b1f293f6deac83b
Yutaka Nagashima <yutacang@googlemail.com>
2019-05-13 12:02:10 +0200

    add Test_LiFtEr.thy

---
+++ MiLkMaId_LiFtEr/Test_LiFtEr.thy
@@ -0,0 +1,12 @@
+(*  Title:      PSL/MiLkMaId_LiFtEr/Test_LiFtEr.thy
+    Author:     Yutaka Nagashima, Czech Technical University in Prague, the University of Innsbruck
+
+MeLoId: Machine Learning Induction for Isabelle/HOL, and
+LiFtEr: Logical Feature Extractor.
+*)
+theory Test_LiFtEr
+  imports
+    "TIP_TO_Test_LiFtEr/Isaplanner/Isaplanner/TIP_prop_01"
+begin
+
+end
