d33247158f4130c40a2b95ca4a5a9706ad64c1a0
Yutaka Nagashima <yutacang@googlemail.com>
2019-05-18 22:19:19 +0200

    more work on pattern_of_a_contract.

--- MiLkMaId_LiFtEr/Test_LiFtEr.thy
+++ MiLkMaId_LiFtEr/Test_LiFtEr.thy
@@ -11,4 +11,18 @@ theory Test_LiFtEr
     "TIP_TO_Test_LiFtEr/TIP15/TIP15/TIP_propositional_AndCommutative"
 begin
 
+(* This definition of "evn" is from the official tutorial "A Proof Assistant for Higher-Order Logic". *)
+inductive evn :: "nat \<Rightarrow> bool" where
+  zero: "evn 0"
+| step: "evn n \<Longrightarrow> evn (Suc (Suc n))"
+
+print_theorems
+
+(* This definition of "evn" is from the official tutorial "A Proof Assistant for Higher-Order Logic". *)
+primrec itrev:: "'a List.list \<Rightarrow> 'a List.list \<Rightarrow> 'a List.list" where
+  "itrev [] ys = ys" |
+  "itrev (xa#xs) ys = itrev xs (xa#ys)"
+
+print_theorems
+
 end
