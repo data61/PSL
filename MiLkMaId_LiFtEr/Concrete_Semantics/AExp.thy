c81cc8f3d7c5d6021bee089f0167e3000240a5e5
Yutaka Nagashima <yutacang@googlemail.com>
2019-05-28 12:33:20 +0200

    add Are_Same_Trm and other modifications.

--- MiLkMaId_LiFtEr/Concrete_Semantics/AExp.thy
+++ MiLkMaId_LiFtEr/Concrete_Semantics/AExp.thy
@@ -20,10 +20,9 @@ type_synonym state = "vname \<Rightarrow> val"
 datatype aexp = N int | V vname | Plus aexp aexp
 
 fun aval :: "aexp \<Rightarrow> state \<Rightarrow> val" where
-"aval (N n) s = n" |
-"aval (V x) s = s x" |
-"aval (Plus a\<^sub>1 a\<^sub>2) s = aval a\<^sub>1 s + aval a\<^sub>2 s"
-
+  "aval (N n) s = n" |
+  "aval (V x) s = s x" |
+  "aval (Plus a\<^sub>1 a\<^sub>2) s = aval a\<^sub>1 s + aval a\<^sub>2 s"
 
 value "aval (Plus (V ''x'') (N 5)) (\<lambda>x. if x = ''x'' then 7 else 0)"
 
@@ -62,38 +61,38 @@ subsection "Constant Folding"
 text{* Evaluate constant subexpressions: *}
 
 fun asimp_const :: "aexp \<Rightarrow> aexp" where
-"asimp_const (N n) = N n" |
-"asimp_const (V x) = V x" |
-"asimp_const (Plus a\<^sub>1 a\<^sub>2) =
+  "asimp_const (N n) = N n" |
+  "asimp_const (V x) = V x" |
+  "asimp_const (Plus a\<^sub>1 a\<^sub>2) =
   (case (asimp_const a\<^sub>1, asimp_const a\<^sub>2) of
     (N n\<^sub>1, N n\<^sub>2) \<Rightarrow> N(n\<^sub>1+n\<^sub>2) |
     (b\<^sub>1,b\<^sub>2) \<Rightarrow> Plus b\<^sub>1 b\<^sub>2)"
 
 theorem aval_asimp_const:
   "aval (asimp_const a) s = aval a s"
-apply(induction a)
-apply (auto split: aexp.split)
-done
+  apply(induction a)
+    apply (auto split: aexp.split)
+  done
 
 text{* Now we also eliminate all occurrences 0 in additions. The standard
 method: optimized versions of the constructors: *}
 
 fun plus :: "aexp \<Rightarrow> aexp \<Rightarrow> aexp" where
-"plus (N i\<^sub>1) (N i\<^sub>2) = N(i\<^sub>1+i\<^sub>2)" |
-"plus (N i) a = (if i=0 then a else Plus (N i) a)" |
-"plus a (N i) = (if i=0 then a else Plus a (N i))" |
-"plus a\<^sub>1 a\<^sub>2 = Plus a\<^sub>1 a\<^sub>2"
+  "plus (N i\<^sub>1) (N i\<^sub>2) = N(i\<^sub>1+i\<^sub>2)" |
+  "plus (N i) a = (if i=0 then a else Plus (N i) a)" |
+  "plus a (N i) = (if i=0 then a else Plus a (N i))" |
+  "plus a\<^sub>1 a\<^sub>2 = Plus a\<^sub>1 a\<^sub>2"
 
 lemma aval_plus [simp]:
   "aval (plus a1 a2) s = aval a1 s + aval a2 s"
-apply(induction a1 a2 rule: plus.induct)
-apply auto
-done
+  apply(induction a1 a2 rule: plus.induct)
+              apply auto
+  done
 
 fun asimp :: "aexp \<Rightarrow> aexp" where
-"asimp (N n) = N n" |
-"asimp (V x) = V x" |
-"asimp (Plus a\<^sub>1 a\<^sub>2) = plus (asimp a\<^sub>1) (asimp a\<^sub>2)"
+  "asimp (N n) = N n" |
+  "asimp (V x) = V x" |
+  "asimp (Plus a\<^sub>1 a\<^sub>2) = plus (asimp a\<^sub>1) (asimp a\<^sub>2)"
 
 
 text{* Note that in @{const asimp_const} the optimized constructor was
@@ -104,8 +103,51 @@ value "asimp (Plus (Plus (N 0) (N 0)) (Plus (V ''x'') (N 0)))"
 
 theorem aval_asimp[simp]:
   "aval (asimp a) s = aval a s"
-apply(induction a)
-apply auto
-done
+  apply(induction a)
+    apply auto
+  done
+
+theorem aval_asimp2:(*A proof without using aval_plus*)
+  "aval (asimp a) s = aval a s"
+  apply(induction a)
+    apply fastforce
+    apply fastforce
+  apply(simp del:aval_plus aval_asimp)
+  (*abstract "asimp a1" and "asimp a2": \<And>a1 a2. aval (asimp a1) s = aval a1 s \<Longrightarrow> aval (asimp a2) s = aval a2 s \<Longrightarrow> aval (AExp.plus (asimp a1) (asimp a2)) s = aval a1 s + aval a2 s*)
+  apply(subgoal_tac "\<And>asimp_a1 asimp_a2. aval (asimp_a1) s = aval a1 s \<Longrightarrow> aval (asimp_a2) s = aval a2 s \<Longrightarrow> aval (AExp.plus (asimp_a1) (asimp_a2)) s = aval a1 s + aval a2 s")
+   apply fastforce
+  apply(case_tac asimp_a1)
+    apply(case_tac asimp_a2)
+      apply (fastforce)
+     apply (fastforce)
+    apply (fastforce)
+   apply(case_tac asimp_a1)
+     apply (fastforce)
+    apply(case_tac asimp_a2)
+      apply fastforce
+     apply fastforce
+    apply fastforce
+   apply fastforce
+  apply(case_tac asimp_a2)
+    apply fastforce
+   apply fastforce
+  apply fastforce
+  done
+
+theorem aval_asimp3:(*A proof without using aval_plus*)
+  "aval (asimp a) s = aval a s"
+  apply(induction a)
+    apply fastforce
+   apply fastforce
+  apply (simp del:aval_plus aval_asimp)
+  apply(subgoal_tac "aval (AExp.plus (asimp a1) (asimp a2)) s = aval (asimp a1) s + aval (asimp a2) s") (* Because "plus a\<^sub>1 a\<^sub>2 = Plus a\<^sub>1 a\<^sub>2"*)
+  apply presburger
+  apply(subgoal_tac "\<And>asimp_a1 asimp_a2. aval (AExp.plus (asimp_a1) (asimp_a2)) s = aval (asimp_a1) s + aval (asimp_a2) s")(* Because the induct_tac cannot apply induction on sub-terms, (asimp a1) and (asimp a2). *)
+  apply fast
+  apply(subgoal_tac "\<forall>s. aval (AExp.plus asimp_a1 asimp_a2) s = aval asimp_a1 s + aval asimp_a2 s") (* Because the induct_tac cannot generalize s with the arbitrary field. *)
+  apply fastforce
+  apply(induct_tac asimp_a1 asimp_a2 rule: plus.induct)
+  apply auto
+  done
 
 end
--- MiLkMaId_LiFtEr/Concrete_Semantics/Induction_Demo.thy
+++ MiLkMaId_LiFtEr/Concrete_Semantics/Induction_Demo.thy
@@ -8,7 +8,7 @@
  *
  *)
 theory Induction_Demo
-imports Main
+  imports Main "../MeLoId_LiFtEr"
 begin
 
 (* HINT FOR ONLINE DEMO
@@ -20,24 +20,205 @@ begin
 *)
 
 fun itrev :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
-"itrev [] ys = ys" |
-"itrev (x#xs) ys = itrev xs (x#ys)"
+  "itrev [] ys = ys" |
+  "itrev (x#xs) ys = itrev xs (x#ys)"
+
+ML{* (* Example assertions in LiFtEr. *)
+local
+
+open LiFtEr_Util LiFtEr;
+infix And Imply Is_An_Arg_Of Is_Rule_Of Is_Nth_Ind Is_In_Trm_Loc Is_In_Trm_Str;
+
+in
+
+(* Example 1-a *)
+val all_ind_term_are_non_const_wo_syntactic_sugar =
+ All_Ind (Trm 1,
+   Some_Trm_Occ (Trm_Occ 1,
+       Trm_Occ_Is_Of_Trm (Trm_Occ 1, Trm 1)
+     And
+       Not (Is_Cnst (Trm_Occ 1)))): assrt;
+
+(* Example 1-b *)
+val all_ind_term_are_non_const_with_syntactic_sugar =
+ All_Ind (Trm 1,
+   Some_Trm_Occ_Of (Trm_Occ 1, Trm 1,
+     Not (Is_Cnst (Trm_Occ 1)))): assrt;
+
+(* Example 2 *)
+val all_ind_terms_have_an_occ_as_variable_at_bottom =
+ All_Ind (Trm 1,
+   Some_Trm_Occ_Of (Trm_Occ 1, Trm 1,
+       Is_Atom (Trm_Occ 1)
+     Imply
+       Is_At_Deepest (Trm_Occ 1)));
+
+(* Example 3 *)
+val all_ind_vars_are_arguments_of_a_recursive_function =
+Some_Trm (Trm 1,
+  Some_Trm_Occ_Of (Trm_Occ 1, Trm 1,
+    All_Ind (Trm 2,
+      Some_Trm_Occ_Of (Trm_Occ 2, Trm 2,
+           Is_Recursive_Cnst (Trm_Occ 1)
+         And
+           (Trm_Occ 2 Is_An_Arg_Of Trm_Occ 1)))));
+
+(* Example 4 *)
+val all_ind_vars_are_arguments_of_a_rec_func_where_pattern_match_is_complete =
+ Not (Some_Rule (Rule 1, True))
+Imply
+ Some_Trm (Trm 1,
+  Some_Trm_Occ_Of (Trm_Occ 1, Trm 1,
+   All_Ind (Trm 2,
+     Some_Trm_Occ_Of (Trm_Occ 2, Trm 2,
+       Is_Recursive_Cnst (Trm_Occ 1)
+      And
+      (Some_Numb (Numb 1,
+         Pattern (Numb 1, Trm_Occ 1, All_Constr)
+        And
+         Is_Nth_Arg_Of (Trm_Occ 2, Numb 1, Trm_Occ 1)))))));
+
+(* Example 5 *)
+val all_ind_terms_are_arguments_of_a_const_with_a_related_rule_in_order =
+ Some_Rule (Rule 1, True)
+Imply
+ Some_Rule (Rule 1,
+  Some_Trm (Trm 1,
+   Some_Trm_Occ_Of (Trm_Occ 1, Trm 1,
+    (Rule 1 Is_Rule_Of Trm_Occ 1)
+    And
+    (All_Ind (Trm 2,
+     (Some_Trm_Occ_Of (Trm_Occ 2, Trm 2,
+       Some_Numb (Numb 1,
+        (Is_Nth_Arg_Of (Trm_Occ 2, Numb 1, Trm_Occ 1)
+        And
+        (Trm 2 Is_Nth_Ind Numb 1))))))))));
+
+(* Example 6 *)
+val ind_is_not_arb =
+All_Arb (Trm 1,
+ Not (Some_Ind (Trm 2,
+  Are_Same_Trm (Trm 1, Trm 2))));
+
+val vars_in_ind_terms_are_generalized =
+ Some_Ind (Trm 1,
+  Some_Trm_Occ_Of (Trm_Occ 1, Trm 1,
+    Some_Trm (Trm 2,
+     Some_Trm_Occ_Of (Trm_Occ 2, Trm 2,
+        (Trm_Occ 2 Is_In_Trm_Loc Trm_Occ 1)
+       And
+        Is_Free (Trm_Occ 2)))))
+Imply
+ Some_Ind (Trm 1,
+  Some_Trm_Occ_Of (Trm_Occ 1, Trm 1,
+    Some_Trm (Trm 2,
+     Some_Trm_Occ_Of (Trm_Occ 2, Trm 2,
+        (Trm_Occ 2 Is_In_Trm_Loc Trm_Occ 1)
+       And
+        Is_Free (Trm_Occ 2)
+       And
+        Some_Arb (Trm 3,
+         Are_Same_Trm (Trm 2, Trm 3))))));
+
+val Example6 = ind_is_not_arb And vars_in_ind_terms_are_generalized;
+
+end;
+*}
+
+setup{* Apply_LiFtEr.update_assert 1 all_ind_term_are_non_const_wo_syntactic_sugar;                           *}
+setup{* Apply_LiFtEr.update_assert 2 all_ind_term_are_non_const_with_syntactic_sugar;                         *}
+setup{* Apply_LiFtEr.update_assert 3 all_ind_terms_have_an_occ_as_variable_at_bottom;                         *}
+setup{* Apply_LiFtEr.update_assert 4 all_ind_vars_are_arguments_of_a_recursive_function;                      *}
+setup{* Apply_LiFtEr.update_assert 5 all_ind_vars_are_arguments_of_a_rec_func_where_pattern_match_is_complete;*}
+setup{* Apply_LiFtEr.update_assert 6 all_ind_terms_are_arguments_of_a_const_with_a_related_rule_in_order;     *}
+setup{* Apply_LiFtEr.update_assert 7 vars_in_ind_terms_are_generalized;                                       *}
+
+ML{* (*Arguments for the induct method to attack "itrev xs ys = rev xs @ ys". *)
+local
+
+open LiFtEr;
+
+in
+
+(* Official solution of induction application by Tobias Nipkow and Gerwin Klein. *)
+val official_solution_for_itrev_equals_rev =
+Ind_Mods
+ {ons   = [Ind_On  (Print "xs")],
+  arbs  = [Ind_Arb (Print "ys")],
+  rules = []
+  }: ind_mods;
+
+val bad_answer_for_itrev_equals_rev =
+Ind_Mods
+ {ons   = [Ind_On  (Print "itrev")],
+  arbs  = [Ind_Arb (Print "ys")],
+  rules = []
+  }: ind_mods;
+
+end;
+*}
+
+setup{* Apply_LiFtEr.update_ind_mod 1 official_solution_for_itrev_equals_rev; *}
+setup{* Apply_LiFtEr.update_ind_mod 2 bad_answer_for_itrev_equals_rev       ; *}
 
 lemma "itrev xs ys = rev xs @ ys"
-apply(induction xs arbitrary: ys)
-apply(auto)
-done
-
+  (*The first argument to assert_LiFtEr_true is the identifier of a LiFtEr assertion, while
+ *the second argument to assert_LiFtEr_true is the identifier of a combination of arguments to
+ *the induct method.*)
+  assert_LiFtEr_true  1 1
+  assert_LiFtEr_false 1 2
+  assert_LiFtEr_true  2 1
+  assert_LiFtEr_false 2 2
+  assert_LiFtEr_true  3 1
+  assert_LiFtEr_true  4 1
+  assert_LiFtEr_true  5 1
+  assert_LiFtEr_true  6 1
+  apply(induction xs arbitrary: ys)
+   apply(auto)
+  done
+    (*alternatively by(induct xs ys rule:itrev.induct) auto*)
 
 subsection{* Computation Induction *}
 
 fun sep :: "'a \<Rightarrow> 'a list \<Rightarrow> 'a list" where
-"sep a [] = []" |
-"sep a [x] = [x]" |
-"sep a (x#y#zs) = x # a # sep a (y#zs)"
+  "sep a [] = []" |
+  "sep a [x] = [x]" |
+  "sep a (x#y#zs) = x # a # sep a (y#zs)"
+
+ML{* (*Arguments for the induct method to attack "map f (sep a xs) = sep (f a) (map f xs)". *)
+local
+
+open LiFtEr;
+
+in
+
+val official_solution_for_map_sep_equals_sep_map =
+Ind_Mods
+ {ons   = [Ind_On  (Print "a"), Ind_On  (Print "xs")],
+  arbs  = [],
+  rules = [Ind_Rule "Induction_Demo.sep.induct"]
+  }: ind_mods;
+
+val only_for_test =
+Ind_Mods
+ {ons   = [Ind_On  (Print "sep a xs")],
+  arbs  = [Ind_Arb (Print "xs")],
+  rules = []
+  }: ind_mods;
+
+end;
+*}
+
+setup{* Apply_LiFtEr.update_ind_mod 3 official_solution_for_map_sep_equals_sep_map; *}
+setup{* Apply_LiFtEr.update_ind_mod 4 only_for_test;                                *}
 
 lemma "map f (sep a xs) = sep (f a) (map f xs)"
-apply(induction a xs rule: sep.induct)
+  assert_LiFtEr_true 3 3
+  assert_LiFtEr_true 4 3
+  assert_LiFtEr_true 5 3
+  assert_LiFtEr_true 6 3
+  assert_LiFtEr_true 7 4
+apply(induction a xs rule: Induction_Demo.sep.induct)
 apply auto
 done
 
