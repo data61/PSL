2dfc5de3fec86a839091dba3603894354d0f22f7
Yutaka Nagashima <yutacang@googlemail.com>
2019-11-21 23:24:04 +0100

    SeLFiE: mk_Quantifier_Domain.

--- SeLFiE_TEMP/SeLFiE.thy
+++ SeLFiE_TEMP/SeLFiE.thy
@@ -31,47 +31,59 @@ ML_file "src/Preprocessor/Dynamic_Induct.ML"
 ML_file "src/Interpreter/Eval_Bool.ML"
 ML_file "src/Interpreter/Eval_Node.ML"
 ML_file "src/Interpreter/Eval_Unode.ML"
-ML_file "src/Interpreter/Path_To_Unode.ML"(*The bifurcation of "inner" and "outer" starts here.*)
-ML_file "src/Interpreter/Print_To_Paths.ML"
+ML_file "src/Interpreter/Eval_Path.ML"(*TODO: Eval_Outer_Path*)
 ML_file "src/Interpreter/Eval_Print.ML"
 ML_file "src/Interpreter/Eval_Number.ML"
-ML_file "src/Interpreter/Eval_Path.ML"(*TODO: Eval_Outer_Path*)
 ML_file "src/Interpreter/Eval_Parameter.ML" (*TODO: Eval_Outer_Path is still Eval_Inner_Parameter.*)
 ML_file "src/Interpreter/Eval_Parameter_With_Bool.ML"
 ML_file "src/Interpreter/Eval_Bound.ML"
 ML_file "src/Interpreter/Eval_Variable.ML"
+ML_file "src/Interpreter/Eval_Quantifier.ML"   (*TODO:Number*)
+ML_file "src/Interpreter/Eval_Deep.ML"
+
+ML_file "src/Interpreter/Path_To_Unode.ML"  (*The bifurcation of "inner" and "outer" starts here.*)
+ML_file "src/Interpreter/Print_To_Paths.ML" (*TODO: These should be produced by a functor.*)
+ML\<open> structure Print_To_Inner_Paths = from_Path_Table_and_Path_To_Unode_to_Print_To_Paths(Inner_Path_Table); \<close>
+ML\<open> structure Print_To_Outer_Paths = from_Path_Table_and_Path_To_Unode_to_Print_To_Paths(Outer_Path_Table); \<close>
 
 ML_file "src/Interpreter/Make_Eval_Path.ML"
-ML\<open> structure Eval_Inner_Path = make_Eval_Path(Inner_Path_To_Inner_Unode): EVAL_PATH; \<close>
-ML\<open> structure Eval_Outer_Path = make_Eval_Path(Outer_Path_To_Outer_Unode): EVAL_PATH; \<close>
-
 ML_file "src/Interpreter/From_Path_To_Parameter.ML"
+ML_file "src/Interpreter/From_Parameter_To_Parameter_With_Bool.ML"
+ML_file "src/Interpreter/From_Parameter_With_Bool_To_Bound.ML"
+ML_file "src/Interpreter/From_Bound_To_Variable.ML"
+ML_file "src/Interpreter/Quantifier_Domain.ML" (*TODO: from_Path_To_Unode_and_Print_To_Paths_to_Quantifier_Domain*)
+ML_file "src/Interpreter/From_Variable_To_Quantifier.ML"
+ML_file "src/Interpreter/From_Quantifier_To_Deep.ML"
+
+ML\<open> structure Eval_Inner_Path = make_Eval_Path(Inner_Path_To_Unode): EVAL_PATH; \<close>
+ML\<open> structure Eval_Outer_Path = make_Eval_Path(Outer_Path_To_Unode): EVAL_PATH; \<close>
+
 ML\<open> structure Eval_Inner_Parameter = from_Path_to_Parameter(Eval_Inner_Path): EVAL_PARAMETER; \<close>
 ML\<open> structure Eval_Outer_Parameter = from_Path_to_Parameter(Eval_Outer_Path): EVAL_PARAMETER; \<close>
 
-ML_file "src/Interpreter/From_Parameter_To_Parameter_With_Bool.ML"
 ML\<open> structure Eval_Inner_Parameter_With_Bool = from_Parameter_to_Parameter_With_Bool (Eval_Inner_Parameter): EVAL_PARAMETER_WITH_BOOL; \<close>
 ML\<open> structure Eval_Outer_Parameter_With_Bool = from_Parameter_to_Parameter_With_Bool (Eval_Outer_Parameter): EVAL_PARAMETER_WITH_BOOL; \<close>
 
-ML_file "src/Interpreter/From_Parameter_With_Bool_To_Bound.ML"
 ML\<open> structure Eval_Inner_Bound = from_Parameter_With_Bool_to_Bound (Eval_Inner_Parameter_With_Bool): EVAL_BOUND; \<close>
 ML\<open> structure Eval_Outer_Bound = from_Parameter_With_Bool_to_Bound (Eval_Outer_Parameter_With_Bool): EVAL_BOUND; \<close>
 
-ML_file "src/Interpreter/From_Bound_To_Variable.ML"
 ML\<open> structure Eval_Inner_Variable = from_Bound_to_Variable (Eval_Inner_Bound): EVAL_VARIABLE; \<close>
 ML\<open> structure Eval_Outer_Variable = from_Bound_to_Variable (Eval_Outer_Bound): EVAL_VARIABLE; \<close>
 
-ML_file "src/Interpreter/Eval_Quantifier.ML"(*TODO:Number*)
-ML_file "src/Interpreter/Quantifier_Domain.ML" (*TODO: from_Path_To_Unode_and_Print_To_Paths_to_Quantifier_Domain*)
+ML\<open> structure Inner_Quantifier_Domain = make_Quantifier_Domain
+     (structure Print_To_Paths           = Print_To_Inner_Paths
+            and Path_To_Unode            = Inner_Path_To_Unode
+            and Eval_Parameter_With_Bool = Eval_Inner_Parameter_With_Bool): QUANTIFIER_DOMAIN \<close>
+ML\<open> structure Outer_Quantifier_Domain = make_Quantifier_Domain
+     (structure Print_To_Paths           = Print_To_Outer_Paths
+            and Path_To_Unode            = Outer_Path_To_Unode
+            and Eval_Parameter_With_Bool = Eval_Outer_Parameter_With_Bool): QUANTIFIER_DOMAIN \<close>
 
-ML_file "src/Interpreter/From_Variable_To_Quantifier.ML"
 ML\<open> structure Eval_Inner_Quantifier = from_Variable_to_Quantifier(structure Eval_Variable = Eval_Inner_Variable and Quantifier_Domain = Inner_Quantifier_Domain): EVAL_QUANTIFIER; \<close>
 ML\<open> structure Eval_Outer_Quantifier = from_Variable_to_Quantifier(structure Eval_Variable = Eval_Outer_Variable and Quantifier_Domain = Outer_Quantifier_Domain): EVAL_QUANTIFIER; \<close>
 
-ML_file "src/Interpreter/Eval_Deep.ML"
-ML_file "src/Interpreter/From_Quantifier_To_Deep.ML"
-
 ML\<open> structure Eval_Inner_Deep = from_Quantifier_to_Deep(structure Eval_Quantifier = Eval_Inner_Quantifier and Quantifier_Domain = Inner_Quantifier_Domain): EVAL_DEEP; \<close>
+ML\<open> structure Eval_Inner_Deep = from_Quantifier_to_Deep(structure Eval_Quantifier = Eval_Outer_Quantifier and Quantifier_Domain = Outer_Quantifier_Domain): EVAL_DEEP; \<close>
 
 ML\<open>
 @{term "let x = 1 in x"};
