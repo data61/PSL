e1e1b39fd6f4585833f38864747939793550bae6
yutakang <Yutaka.Nagashima@cvut.cz>
2020-04-25 17:33:05 +0900

    PGT: undo a wrongly added change after a demo.

--- PGT/Example.thy
+++ PGT/Example.thy
@@ -5,21 +5,14 @@ begin
 (* DEMO1: PSL *)
 
 fun sep::"'a \<Rightarrow> 'a list \<Rightarrow> 'a list" where
-  "sep a []       = []" |
-  "sep a [x]      = [x]" |
+  "sep a [] = []" |
+  "sep a [x] = [x]" |
   "sep a (x#y#zs) = x # a # sep a (y#zs)"
 
-value "sep (1::int) [0,0,0]"
-
-strategy DInd  = Thens [Dynamic (Induct), Auto, IsSolved]
-
+(* DEMO2: PaMpeR *)
 lemma
   "map f (sep x xs) = sep (f x) (map f xs)"
-  find_proof DInd
-
-
-
-
+  (* Before PaMpeR *)
   print_methods
     (* With PaMpeR *)
   which_method
@@ -28,31 +21,26 @@ lemma
   rank_method coinduction
   oops
 
+strategy DInd = Thens [Dynamic (Induct), Auto, IsSolved]
+
 lemma "map f (sep x xs) = sep (f x) (map f xs)"
-(*
   find_proof DInd
   try_hard
-*)
   oops
 
 (* DEMO3: PGT *)
-primrec rev:: "'a list \<Rightarrow> 'a list" where
-  "rev []       = []"
-| "rev (x # xs) = rev xs @ [x]"
+
 primrec itrev:: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
-  "itrev [] ys     = ys"
-| "itrev (x#xs) ys = itrev xs (x#ys)"
+  "itrev [] ys = ys" |
+  "itrev (x#xs) ys = itrev xs (x#ys)"
 
-strategy DInd  = Thens [Dynamic (Induct), Auto, IsSolved]
-
-
-
-lemma "itrev xs [] = rev xs"
-  find_proof DInd
-
+thm rev.simps
 
 strategy CDInd = Thens [Conjecture, Fastforce, Quickcheck, DInd]
 strategy DInd_Or_CDInd = Ors [DInd, CDInd]
+
+lemma "itrev xs [] = rev xs"
+(*  DInd cannot find a proof script: find_proof DInd *)
   find_proof DInd_Or_CDInd
   oops
 
