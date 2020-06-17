5a830e66f1d8e67260a3e50da2dcff052c9600a9
yutakang <Yutaka.Nagashima@cvut.cz>
2020-06-17 15:23:16 +0900

    SeLFiE: define_recursively for not only for node but also for print.

--- SeLFiE/Example/Test_Some_AFPs.thy
+++ SeLFiE/Example/Test_Some_AFPs.thy
@@ -2,6 +2,7 @@ theory Test_Some_AFPs
   imports
     "afp-2020-05-16/thys/KD_Tree/Nearest_Neighbors"
     "afp-2020-05-16/thys/Hybrid_Logic/Hybrid_Logic"
+    "afp-2020-05-16/thys/Goodstein_Lambda/Goodstein_Lambda"
 begin
 
 end
